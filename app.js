f93ded7538b1c636d08a305afedffade846b5245
özcan çetin <cetin445@gmail.com>
2022-06-08 13:22:05 +0300

    capturing method created and "ac" button completed

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 let firstScreen=document.querySelector(".firstScreen")
 let secondScreen=document.querySelector(".secondScreen")
 let buttons=document.querySelector(".buttons");
+
+/*CONDITIONALS*/
+buttons.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("buttons")){
+        null
+    }else{
+        if(e.target.classList.contains("ac")){
+            e.target.closest(".container").querySelector(".firstScreen").innerText="";
+            e.target.closest(".container").querySelector(".secondScreen").innerText="";
+        }
+        
+    }
+})
